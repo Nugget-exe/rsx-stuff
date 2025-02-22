a53b0c906dfb65ec44344eb84d700869e5fd97a1
Ken-2511 <iwmain@outlook.com>
2025-02-22 16:48:11 -0500

    ...

--- arduinotest.py
+++ arduinotest.py
@@ -1,14 +1,22 @@
-
-# Importing Libraries
 import serial
 import time
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+import serial.tools.list_ports
+
+ports = serial.tools.list_ports.comports()
+for port in ports:
+	print(port.device)
+port = ports[0].device
+arduino = serial.Serial(port=port, baudrate=115200, timeout=.1)
+
+
 def write_read(x):
 	arduino.write(bytes(x, 'utf-8'))
 	time.sleep(0.05)
-	data = arduino.readline()
-	return data
+	response = arduino.readline()
+	return response
+
+
 while True:
-	num = input("Enter a number: ") # Taking input from user
-	value = write_read(num)
+	command = input("Enter your command: ") # Taking input from user
+	value = write_read(command + '\n')
 	print(value) # printing the value
